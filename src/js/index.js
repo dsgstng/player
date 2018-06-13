94d4b7f0f58d027f63ae90017296b08390da514a
ejnshtein <ejnshtein@gmail.com>
2018-06-13 21:07:34 +0300

    v0.0.5

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import '../css/index.scss';
 import DsgstngPlayer from './player';
 
 /* global DPLAYER_VERSION GIT_HASH */
-console.log(`${'\n'} %c dsgstng-player v${DPLAYER_VERSION} ${GIT_HASH} %c https://dsgstng.com ${'\n'}${'\n'}`, 'color: var(--main-theme-color); background: var(--main-background); padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+console.log(`${'\n'} %c DsgstngPlayer v${DPLAYER_VERSION} ${GIT_HASH} %c https://dsgstng.com ${'\n'}${'\n'}`, 'color: var(--main-theme-color); background: var(--main-background); padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
 
 export default DsgstngPlayer;
